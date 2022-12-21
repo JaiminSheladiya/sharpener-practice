ae11ff2c8787cea273e7239a2d680682bca78950
jaimins365635 <jaimins365635@gmail.com>
2022-12-21 19:54:03 +0530

    creating Nodes and Modifying Dom assignment

--- getElementByClassName/index.js
+++ getElementByClassName/index.js
@@ -1,12 +1,48 @@
-// QuerySelector & QuerySelectorAll
+let li = document.querySelector('#items')
 
-let li = document.querySelector('li:nth-child(2)')
-li.style.backgroundColor = 'green'
-document.querySelector('li:nth-child(3)').style.display = 'none'
+// parentNode
+// li.parentNode.style.backgroundColor = '#f4f4f4'
 
+// parentElement
+// li.parentElement.style.backgroundColor = '#f4f4f4'
 
-let list = document.querySelectorAll('li')
-list[1].style.backgroundColor = 'green'
-for(let i = 0;i<list.length;i++){
-    if(i%2===0)list[i].style.backgroundColor = 'green'
-}
+// childNodes
+
+// console.log(li.childNodes)
+// console.log(li.children)
+// li.children[1].style.color='yellow'
+
+// firstChild
+// console.log(li.firstChild)
+// firstElementChild
+// console.log(li.firstElementChild)
+// li.firstElementChild.textContent = 'Hello 1'
+
+// lastChild
+// console.log(li.lastChild)
+// lastElementChild
+// console.log(li.lastElementChild)
+// li.lastElementChild.textContent = 'Hello last'
+
+// nextSibling
+// console.log(li.nextSibling)
+// nextElementSibling
+// console.log(li.nextElementSibling)
+
+// previousSibling
+// console.log(li.previousSibling)
+// console.log(li.previousElementSibling)
+// li.previousElementSibling.style.color = 'red'
+
+// createElement
+
+let newDiv = document.createElement('div')
+newDiv.className = 'hello'
+newDiv.id = 'hello1'
+newDiv.setAttribute('title', 'Hello Div')
+var newDivText = document.createTextNode('Hello World')
+newDiv.appendChild(newDivText)
+let container = document.querySelector('header .container')
+let h1 = document.querySelector('header h1')
+console.log(newDiv)
+container.insertBefore(newDiv,h1)
