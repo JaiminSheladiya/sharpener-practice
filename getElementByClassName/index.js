f4218aa6fe9ec590a78550c1e6d57f6cfc42acf5
jaimins365635 <jaimins365635@gmail.com>
2022-12-21 18:44:38 +0530

    getElementByClassName assignment

---
+++ getElementByClassName/index.js
@@ -0,0 +1,21 @@
+// console.log(document.all[10])
+// document.all[10].textContent = 'Hello'
+// console.log(document.images)
+
+
+ let headerTitle = document.getElementById('header-title')
+//  headerTitle.innerText = 'Hello'
+// headerTitle.innerHTML = '<h3>Hello H3 </h3>'
+
+// headerTitle.style.borderBottom = 'solid 3px white'
+
+// getElementByClassName :-
+
+var items = document.getElementsByClassName('list-group-item')
+console.log(items)
+items[1].textContent = 'Helloo 2'
+for(let i = 0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold'
+    items[i].style.backgroundColor = '#f4f4f4'
+}
+items[2].style.backgroundColor = 'green'
