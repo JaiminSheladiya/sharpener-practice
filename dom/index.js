3efa838d222879236802025576c3073976be0745
jaimins365635 <jaimins365635@gmail.com>
2022-12-21 12:51:27 +0530

    dom assignment

---
+++ dom/index.js
@@ -0,0 +1,16 @@
+// Examine the document object
+console.dir(document)
+console.log(document.domain)
+console.log(document.URL)
+console.log(document.title)
+// document.title = 'My dom assignment'
+console.log(document.doctype)
+console.log()
+console.log(document.head)
+console.log(document.body)
+console.log(document.all)
+console.log(document.all[10])
+
+
+let h1 = document.getElementById('h1')
+h1.innerText='Jai ho'
