971737c300406c16cf4f421f475dd8bca4fb0c56
jaimins365635 <jaimins365635@gmail.com>
2022-12-22 01:36:07 +0530

    filter assignment done

--- getElementByClassName/index.js
+++ getElementByClassName/index.js
@@ -1,11 +1,31 @@
 let form = document.getElementById('addForm')
 let itemList = document.getElementById('items')
-
+let items = itemList.getElementsByTagName('li')
+    let arr = Array.from(items)
 // Form submit event
 // add item
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
     let newItem = document.getElementById('item')
+    let desc = document.getElementById('desc').value
+
+    let span = document.createElement('span')
+    span.className = 'ml-3'
+    span.innerText = desc
+
+    console.log(arr)
+    let text = newItem.value.toLowerCase()
+    arr.forEach(function(item){
+        let itemName = item.firstChild.textContent
+        if(itemName.toLowerCase().indexOf(text) != -1) {
+            item.append(desc)
+        }
+        else{
+            li.append(span)
+
+        }
+    })
+
 
     // create new li element
     let li = document.createElement('li')
@@ -21,8 +41,11 @@ form.addEventListener('submit', (e)=>{
     let edit = document.createElement('button')
     edit.className = 'btn btn-primary float-right btn-sm mr-3'
     edit.innerText='EDIT'
+
+   
     li.appendChild(btn)
     li.appendChild(edit)
+
 
     itemList.appendChild(li)
 })
@@ -38,3 +61,15 @@ itemList.addEventListener('click',(e)=>{
     }
 })
 
+// Filter Items
+let filter = document.getElementById('filter')
+filter.addEventListener('keyup',(e)=>{
+    let text = e.target.value.toLowerCase()
+
+   arr.forEach(function(item){
+        let itemName = item.textContent
+        if(itemName.toLowerCase().indexOf(text) != -1) item.style.display = 'block'
+        else item.style.display = 'none'
+    })
+    
+})
