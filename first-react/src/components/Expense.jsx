8bf42faa8866fb25e6b0fdf0d1f616665fa42fea
jaimins365635 <jaimins365635@gmail.com>
2022-12-31 01:58:49 +0530

    react app first draft

---
+++ first-react/src/components/Expense.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Expense = () => {
+  return (
+      <div>
+          <p>This is expense.</p>
+    </div>
+  )
+}
+
+export default Expense
---
+++ first-react/src/components/ExpenseItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Expense from './Expense'
+
+const ExpenseItem = () => {
+  return (
+    <>
+      <h2>Expense Item</h2>
+      <Expense />
+      <Expense />
+    </>
+  );
+}
+
+export default ExpenseItem
