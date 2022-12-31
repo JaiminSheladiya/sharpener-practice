ed82d9f03f8cf7830de739bcfa9347e533fd6491
jaimins365635 <jaimins365635@gmail.com>
2022-12-31 17:27:40 +0530

    Passing Data via props assignment done

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,11 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: "Delhi",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "Pune",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: "Bangalore",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: "Ahmedabad",
+    },
+  ];
   return (
     <div className="App">
-     <ExpenseItem />
+      {expenses.map((e) =>  
+          <ExpenseItem
+            title={e.title}
+            amount={e.amount}
+            date={e.date}
+            LocationOfExpenditure={e.location}
+          />
+        )}
+      
     </div>
   );
 }
--- first-react/src/components/ExpenseItem.jsx
+++ first-react/src/components/ExpenseItem.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-import Expense from "./Expense";
 import "./ExpenseItem.css";
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-  const LocationOfExpenditure = 'Manali'
+const ExpenseItem = ({title , amount , date , LocationOfExpenditure}) => {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>{date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <h2>{LocationOfExpenditure}</h2>
-        <div className="expense-item__price">$ {expenseAmount}</div>
+        <div className="expense-item__price">$ {amount}</div>
       </div>
     </div>
   );
-};
-
+}
 export default ExpenseItem;
