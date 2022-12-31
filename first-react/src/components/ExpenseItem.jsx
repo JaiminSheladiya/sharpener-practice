5ccd53478ac9b8dfe32763c423207a69ea0813a3
jaimins365635 <jaimins365635@gmail.com>
2022-12-31 12:29:52 +0530

    adding css to react project

--- first-react/src/components/ExpenseItem.jsx
+++ first-react/src/components/ExpenseItem.jsx
@@ -5,12 +5,13 @@ const ExpenseItem = () => {
   const expenseDate = new Date(2021, 2, 28);
   const expenseTitle = "Car Insurance";
   const expenseAmount = 294.67;
-
+  const LocationOfExpenditure = 'Manali'
   return (
     <div className="expense-item">
       <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
+        <h2>{LocationOfExpenditure}</h2>
         <div className="expense-item__price">$ {expenseAmount}</div>
       </div>
     </div>
