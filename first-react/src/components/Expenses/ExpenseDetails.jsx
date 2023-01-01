25fcd73817d79180221721d33de63785b937115a
jaimins365635 <jaimins365635@gmail.com>
2023-01-01 17:47:46 +0530

    Adding event listner made easy

--- first-react/src/components/Expenses/ExpenseDetails.jsx
+++ first-react/src/components/Expenses/ExpenseDetails.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './ExpenseItem.css'
 const ExpenseDetails = ({ title, amount, LocationOfExpenditure }) => {
+  function deleteExpense() {
+  console.log('Deleted !') 
+ }
   return (
     <div className="expense-item__description">
       <h2>{title}</h2>
       <h2>{LocationOfExpenditure}</h2>
       <div className="expense-item__price">$ {amount}</div>
+      <button onClick={deleteExpense} className="expense-item__price">Delete Expense</button>
     </div>
   );
 }
