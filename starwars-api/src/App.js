9dbd76229ab5b440e589a64292dbc04cfae905ad
jaimins365635 <jaimins365635@gmail.com>
2023-01-07 16:07:50 +0530

    Making apps error proof done

--- starwars-api/src/App.js
+++ starwars-api/src/App.js
@@ -1,19 +1,39 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [loading,setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const ref = useRef(true)
+  // console.log(e)
   async function fetchMoviesHandler() {
     setLoading(true)
-   const res = await fetch("https://swapi.dev/api/films/")
-    const data = await res.json()
-    
-        const transformedMovies = data.results.map(movieData => { return { id: movieData.episode_id, title: movieData.title, openingText: movieData.opening_crawl, releaseDate: movieData.release_date } })
-        setMovies(transformedMovies);
-setLoading(false)    
+    setError(null)
+    try {
+       const res = await fetch("https://swapi.dev/api/films/");
+       const data = await res.json();
+
+      if(!res.ok) throw new Error('Something went wrong! ...Retrying')
+      
+       const transformedMovies = data.results.map((movieData) => {
+         return {
+           id: movieData.episode_id,
+           title: movieData.title,
+           openingText: movieData.opening_crawl,
+           releaseDate: movieData.release_date,
+         };
+       });
+       setMovies(transformedMovies);
+       setLoading(false);    
+    } catch (error) {
+      setError(error.message + ' Retrying in 5 seconds')
+      if(!ref.current) setError(error.message)
+      if (ref.current) setTimeout(() => fetchMoviesHandler(), 5000)
+    }
+      setLoading(false);
   }
 
   return (
@@ -23,6 +43,8 @@ setLoading(false)
       </section>
       <section>
        {loading? <p>FETCHING...</p> :<MoviesList movies={movies} />} 
+        {!loading && error && <p>{error}</p>}
+        {!loading && error && ref.current && <button onClick={()=>ref.current = false}>Cancel</button>}
       </section>
     </React.Fragment>
   );
