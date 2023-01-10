5475c0ba97925679ac11f2e090b81a0f79a93664
jaimins365635 <jaimins365635@gmail.com>
2023-01-10 14:24:49 +0530

    Edit User Details

--- react-expense-tracker/src/components/CompleteProfile/CompleteProfilePage.jsx
+++ react-expense-tracker/src/components/CompleteProfile/CompleteProfilePage.jsx
@@ -1,41 +1,56 @@
-import axios from 'axios';
-import React, { useState } from 'react'
-import { toast } from 'react-toast';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toast";
 
 const CompleteProfilePage = () => {
+  const [defaultData, setDefaultData] = useState({ fullName: "", url: "" });
+  const [userData, setUserData] = useState({
+    fullName: "",
+    url: "",
+  });
+  useEffect(() => {
+getPreviousValues();
+  },[])
 
-     const [userData, setUserData] = useState({
-       fullName: "",
-       url: "",
-     });
-    
-    const handleChange = (e) => {
-      const { placeholder, value } = e.target;
-      setUserData({ ...userData, [placeholder]: value });
-    };
+  async function getPreviousValues() {
+    let idToken = localStorage.getItem("idToken");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        console.log(userData)
-        let idToken = localStorage.getItem("idToken");
-        try {
-               const res = await axios.post(
-          "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAAXV5Vs62BDdmIhqYmBwYQ_embaTIn_o0",
-          {
-            idToken: idToken,
-            displayName: userData.fullName,
-            photoUrl : userData.url
-          }
-        );
-        toast('Profile Updated')
-    document.querySelector("form").reset();
-        } catch (e) {
-              toast(e.response.data.error.message);
+    const res = await axios.post(
+      "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyAAXV5Vs62BDdmIhqYmBwYQ_embaTIn_o0"
+      , { idToken: idToken });
+    setDefaultData({
+      fullName: res.data.users[0].displayName,
+      url: res.data.users[0].photoUrl,
+    });
+  }
+
+  const handleChange = (e) => {
+    const { placeholder, value } = e.target;
+    setUserData({ ...userData, [placeholder]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(userData);
+    let idToken = localStorage.getItem("idToken");
+    try {
+      const res = await axios.post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAAXV5Vs62BDdmIhqYmBwYQ_embaTIn_o0",
+        {
+          idToken: idToken,
+          displayName: userData.fullName,
+          photoUrl: userData.url,
         }
-     
+      );
 
+      toast("Profile Updated");
+      document.querySelector("form").reset();
+    } catch (e) {
+      console.log(e);
+      toast(e.response.data.error.message);
     }
-    
+  };
+
   return (
     <div>
       <form
@@ -48,6 +63,7 @@ const CompleteProfilePage = () => {
             className="form-control"
             onChange={handleChange}
             placeholder="fullName"
+            defaultValue={defaultData.fullName}
             required
           />
           <label>Full Name</label>
@@ -59,6 +75,7 @@ const CompleteProfilePage = () => {
             className="form-control"
             onChange={handleChange}
             placeholder="url"
+            defaultValue={defaultData.url}
             required
           />
           <label>Profile Photo URL</label>
@@ -68,6 +85,6 @@ const CompleteProfilePage = () => {
       </form>
     </div>
   );
-}
+};
 
-export default CompleteProfilePage
+export default CompleteProfilePage;
