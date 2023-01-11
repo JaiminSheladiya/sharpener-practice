261b1ed52f66b8871d1dd2eebe30abba3266f25e
jaimins365635 <jaimins365635@gmail.com>
2023-01-12 00:29:54 +0530

    redux-demo

---
+++ redux-intro/redux-demo.js
@@ -0,0 +1,24 @@
+const redux = require('redux')
+
+const counterReducer = (state = {counter : 0}, {type,payload}) => {
+    if (type == 'increment') return {
+        counter: state.counter + payload
+    }
+    else if (type == 'decrement') return { counter: state.counter - payload }
+        return {
+        counter : state.counter 
+    }
+}
+
+const store = redux.createStore(counterReducer)
+
+
+
+const counterSubscriber = () => {
+    const latestState = store.getState()
+    console.log(latestState)
+}
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment", payload: 5 });
+store.dispatch({ type: "decrement", payload: 2});
