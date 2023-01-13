10e4791db513fb7dffaf76e66e4b789747d25bf6
jaimins365635 <jaimins365635@gmail.com>
2023-01-13 15:41:28 +0530

    Doing Network calls and andling response the right way

--- redux-refresher/src/App.js
+++ redux-refresher/src/App.js
@@ -1,16 +1,71 @@
-import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data.",
+        })
+      );
+      const res = await fetch(
+        "https://react-http-8f419-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!res.ok) {
+        dispatch(
+          uiActions.showNotification({
+            status: "error",
+            title: "Error!",
+            message: "Sending cart data failed.",
+          })
+        );
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+
+      const data = await res.json();
+    };
+    sendCartData();
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          title={notification.title}
+          status={notification.status}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- redux-refresher/src/components/Shop/ProductItem.js
+++ redux-refresher/src/components/Shop/ProductItem.js
@@ -1,12 +1,26 @@
-import { useDispatch } from 'react-redux';
-import { cartActions } from '../../store/cart-slice';
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch, useSelector } from "react-redux";
+
+import { cartActions } from "../../store/cart-slice";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
-  const { title, price, description , id } = props;
-  const dispatch = useDispatch()
-  
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const { title, price, description, id } = props;
+
+  const addToCartHandler = () => {
+
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+
   return (
     <li className={classes.item}>
       <Card>
@@ -16,7 +30,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button onClick={() => dispatch(cartActions.addItemToCart({id,title,price}))}>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
